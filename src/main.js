acffe31d64b63431b796e9be1e69505dcd779b10
Herman <smurovnemez@mail.ru>
2022-10-17 23:43:34 -0500

    init: project setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import './assets/main.css'
+import './assets/tailwind.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: {
+        title: 'IP Address Tracker'
+      }
+    },
+   
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next()
+})
+
+export default router
