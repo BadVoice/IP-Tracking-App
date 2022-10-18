4fb6985423cc4a692efa862934f94222795d9849
Herman <smurovnemez@mail.ru>
2022-10-18 09:15:41 -0500

    feat: leaflet map immigration

---
+++ src/components/IPInfo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="absolute  
+    top-1/4 left-1/2
+    transform -translate-x-1/2 -translate-y-1/2
+    flex 
+    flex-col 
+    lg:flex-row 
+    bg-white
+    gap-y-4
+    gap-x-12
+    px-8 pt-8 pb-12
+    rounded-md
+    ">
+    <!-- Flex Item #1 -->
+        <div class="flex flex-col ">
+            <h3 class="text-xs mb-2 uppercase">IP Address</h3>
+            <span class="text-2xl">8.8.8.8</span>
+        </div>
+        <!-- Flex Item #2 -->
+        <div class="flex flex-col ">
+            <h3 class="text-xs mb-2 uppercase">Location</h3>
+            <span class="text-2xl">Michigan</span>
+        </div>
+        <!-- Flex Item #3 -->
+        <div class="flex flex-col ">
+            <h3 class="text-xs mb-2 uppercase">Timezone</h3>
+            <span class="text-2xl">UTC -7:00</span>
+        </div>
+        <!-- Flex Item #4 -->
+        <div class="flex flex-col ">
+            <h3 class="text-xs mb-2 uppercase">Ips</h3>
+            <span class="text-2xl">Google</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
   <div class="flex flex-col h-screen max-h-screen">
 <!-- Search / Results-->
     <div 
-    class="flex justify-center realtive bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+    class=" z-20 flex justify-center realtive bg-hero-pattern bg-cover px-4 pt-8 pb-32"
     
     >
 <!-- Search / input-->
@@ -22,12 +22,43 @@
             flex items-center "></i>
         </div>
       </div>
+<!-- IP Info--> 
+      <IPInfo />
     </div>
+
+<!-- Map -->
+    <div id="mapid" class="h-full z-10"></div>
   </div>
 </template>
 
 
 <script setup>
+import IPInfo from '../components/IPInfo.vue';
+import leaflet from 'leaflet'
+import { onMounted } from '@vue/runtime-core';
+
+let mymap;
+
+  onMounted(() => {
+    mymap = leaflet.map("mapid").setView([42.5145, -83.0147], 9);
+      leaflet
+        .tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmFkdm8xY2UiLCJhIjoiY2w2dXdneXd5MDRtbjNjcGZlamhndzFjZyJ9.xYf7TB6PpSezpB6AOyQSQQ",
+          {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+              "pk.eyJ1IjoiYmFkdm8xY2UiLCJhIjoiY2w2dXdneXd5MDRtbjNjcGZlamhndzFjZyJ9.xYf7TB6PpSezpB6AOyQSQQ",
+          }
+        )
+        .addTo(mymap);
+  
+      })
+
+        
 
 </script>
 
